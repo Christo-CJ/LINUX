e6d0aa5fb277e839cff4e8e1056f6377e0ae271b
Christo Jeeson <68178794+Christo-CJ@users.noreply.github.com>
2025-01-15 22:58:05 +0530

    Update RAND_program.c

--- Day 5/RAND_program.c
+++ Day 5/RAND_program.c
@@ -4,7 +4,7 @@ void main()
 {
   int a[5];
   int i;
-  Srand(getpid()); // giving pid orr seed to rand
+  Srand(getpid()); // giving pid orr seed to rand  
   for(i=0;i<5;i++)
     a[i]=rand()%101-50; // -50 to 50
     //a[i]=rand()%101-100; //100-200  
